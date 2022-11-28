5f09bacdaa1ac2eca07588f9681cb6144a013a1c
Angel Travieso <angeltraviesoc@gmail.com>
2022-11-28 15:03:53 -0400

    radio buttons

--- app/src/main/java/com/example/widgetsapp/MainActivity.java
+++ app/src/main/java/com/example/widgetsapp/MainActivity.java
@@ -6,12 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     // declaring widgets
     CheckBox box1, box2;
+    RadioButton radioButton1, radioButton2;
     Button btn;
 
     @Override
@@ -20,8 +22,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // initialize widgets
+
+        // Checkbox
         box1 = findViewById(R.id.checkbox1);
         box2 = findViewById(R.id.checkbox2);
+
+        // Radio button
+        radioButton1 = findViewById(R.id.radioButton);
+        radioButton2 = findViewById(R.id.radioButton2);
+
+        // tap of the radio button
+        radioButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "With Delivery option selected", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        radioButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Without Delivery option selected", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Button
         btn = findViewById(R.id.btn);
 
         // handle tap events
@@ -36,14 +61,10 @@ public class MainActivity extends AppCompatActivity {
                 // verify if the checkbox is checked
                 if(box1.isChecked()) {
                     Toast.makeText(MainActivity.this, "Cheese Topping is added", Toast.LENGTH_SHORT).show();
-                } else {
-
                 }
 
                 if(box2.isChecked()) {
                     Toast.makeText(MainActivity.this, "Tomato Topping is added", Toast.LENGTH_SHORT).show();
-                } else {
-
                 }
             }
         });
